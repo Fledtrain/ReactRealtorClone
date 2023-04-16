ab387d861b4ce0ea2219c1d3044f42152be44efa
Fledtrain <Joeyew2014@gmail.com>
2023-04-16 15:43:49 -0400

    Installed Reactjs and tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl bg-red-500 ">Hello World</h1>
+      <p className="text-3l">Hi</p>
+    </>
+  );
+}
+
+export default App;
