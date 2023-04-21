266be6e669d693f4b7cb6d87d206824c7d8ce5ce
Fledtrain <Joeyew2014@gmail.com>
2023-04-21 00:31:12 -0400

    added the edit functionality to the Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateCurrentUser, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { doc, updateDoc } from "firebase/firestore";
 
 const Profile = () => {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -13,6 +17,31 @@ const Profile = () => {
   const onLogout = () => {
     auth.signOut();
     navigate("/");
+  };
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...formData,
+      [e.target.id]: e.target.value,
+    }));
+  };
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // Update the user's display name in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        // Update the name in firestore
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Profile name changed successfully");
+    } catch (error) {
+      toast.error("Could not save changes");
+      console.log(error.message);
+    }
   };
 
   return (
@@ -23,12 +52,15 @@ const Profile = () => {
           <form>
             {/* Name Input */}
             <input
-              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 
-              rounded transition ease-in-out"
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 
+              rounded transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}
               type="text"
               id="name"
               value={name}
-              disabled
+              disabled={!changeDetail}
+              onChange={onChange}
             />
             {/* Email Input */}
             <input
@@ -45,10 +77,14 @@ const Profile = () => {
                 Do you want to change your name?
               </p>
               <span
+                onClick={() => {
+                  changeDetail && onSubmit();
+                  setChangeDetail((prevState) => !prevState);
+                }}
                 className="text-red-600 hover:text-red-800 
                 transition ease-in-out duration-200 mr-20 cursor-pointer"
               >
-                Edit
+                {changeDetail ? "Apply change" : "Edit"}
               </span>
               <p
                 onClick={onLogout}
