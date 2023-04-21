e30ef03679e675aa79dca2f690f56c5f92d2875d
Fledtrain <Joeyew2014@gmail.com>
2023-04-21 12:47:40 -0400

    Complete the header component and made it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Header = () => {
+  // Check the status of the user
+  const [pageState, setPageState] = useState("Sign in");
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile");
+      }else{
+        setPageState("Sign in");
+      }
+    });
+  }, [auth]);
+
   const location = useLocation();
   const navigate = useNavigate();
   const pathMatchRoute = (route) => {
@@ -39,10 +53,13 @@ const Header = () => {
               </li>
               <li
                 className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-                ${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}`}
-                onClick={() => navigate("/sign-in")}
+                ${
+                  (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+                  "text-black border-b-red-500"
+                }`}
+                onClick={() => navigate("/profile")}
               >
-                Sign in
+                {pageState}
               </li>
             </ul>
           </div>
